a9da6439b6c899b23694283a0847cce52c7783b0
kim <Kimvz19@users.noreply.github.com>
2024-11-18 13:05:05 +0100

    adding svelte page

---
+++ src/components/header.svelte
@@ -0,0 +1,61 @@
+<!-- adding fonts-->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet">
+
+
+<!-- html code-->
+<h1>De psychologische impact van COVID-19</h1>
+<h2>Breng de indicatoren van angst en depressie in beeld</h2>
+
+<!-- filter buttons -->
+<p>Filter opties</p>
+
+<div class="dropdown">
+    <div class="dropdown-content">
+        <button>all filters</button>
+        <button>time period</button>
+        <button>state</button>
+        <button>gender</button>
+    </div>
+</div>
+
+<style>
+
+    h1, h2{
+        display: flex;
+        justify-content: center;
+    }
+    
+    h1{
+      font-family: "Montserrat";
+      font-optical-sizing: auto;
+      font-weight: 300;
+      font-style: normal;
+      font-size: 45px;
+    }
+    
+    h2, p{
+      font-family: "Montserrat";
+      font-optical-sizing: auto;
+      font-weight: 200;
+      font-style: normal;
+      font-size: 25px;
+    }
+    
+    p{
+    margin-left: 10px;
+    }
+    
+    button{
+      font-family: "Montserrat";
+      font-optical-sizing: auto;
+      font-weight: 200;
+      font-style: normal;
+      font-size: 20px;
+      margin-left: 10px;
+    }
+    
+    </style>
