98987ba96341b3ff223c8c31956c87cfe0a65d37
kim <Kimvz19@users.noreply.github.com>
2024-11-20 11:26:59 +0100

    cleaning up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,6 @@
 <script>
     import Header from "../components/header.svelte";
     import RadarChart from '../components/RadarChart.svelte';
-    import Filteroptions from "../components/filteroptions.svelte";
     import Filter from "../components/filter.svelte";
     import Time from "../components/Timefilter.svelte";
 
@@ -13,7 +12,6 @@
     <Header />
     <Filter />
     <RadarChart />
-    <Filteroptions />
     <Time />
 </main>
 
