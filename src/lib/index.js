37a0f6a379cda1c7301b90fda193396624d2985c
kim <Kimvz19@users.noreply.github.com>
2024-11-22 11:54:56 +0100

    updating

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,14 +1,3 @@
-import adapter from '@sveltejs/adapter-vercel';
- 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  kit: {
-    adapter: adapter({
-      runtime: 'nodejs18.x',
-    }),
-  },
-};
- 
-export default config;
+
 
 
