1e5cf8b15ad75b2754cf33d390864774d7a4589b
kim <Kimvz19@users.noreply.github.com>
2024-11-22 14:32:20 +0100

    updating

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,13 @@
 <script>
     import Header from "../components/header.svelte";
     import RadarChart from "../components/RadarChart.svelte";
-    import State from "../components/Statefilter.svelte";
+    // import State from "../components/Statefilter.svelte";
     import Tryout from "../components/tryout.svelte";
 </script>
 
 <main>
     <Header />
-    <State />
+    <!-- <State /> -->
     <Tryout />
     <RadarChart />
 </main>
