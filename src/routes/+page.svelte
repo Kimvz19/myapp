62635e3fc63463d7064b1772da97cb40a34d3f49
kim <Kimvz19@users.noreply.github.com>
2024-11-25 12:44:22 +0100

    cleaning up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,11 @@
 <!-- imports van componenten-->
 <script>
     import Header from "../components/header.svelte";
-  
-    // import State from "../components/Statefilter.svelte";
     import Tryout from "../components/tryout.svelte";
-    // import RadarChart from "../components/RadarChart.svelte";
 </script>
 
 
 <main>
     <Header />
-    <!-- <State /> -->
     <Tryout />
-    <!-- <RadarChart/> -->
 </main>
