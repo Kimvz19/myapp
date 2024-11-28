dbef10b5fd017753e46cc0a901d2fb26415726a1
kim <Kimvz19@users.noreply.github.com>
2024-11-28 09:53:42 +0100

    cleaning up code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,12 @@
 <script>
     import Header from "../components/header.svelte";
     import Main from "../components/main.svelte";
+    import Footer from "../components/footer.svelte";
 </script>
 
 
 <main>
     <Header />
     <Main />
+    <Footer />
 </main>
