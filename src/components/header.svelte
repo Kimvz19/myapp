286aee9503a4076819210672409eaea1f14a9711
kim <Kimvz19@users.noreply.github.com>
2024-11-28 10:08:56 +0100

    adding styling header

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,29 +1,49 @@
-<!-- adding fonts-->
+<!-- Toevoegen van fonts -->
+<!-- fonts van google -->
+
 <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+
+<!-- Montserrat weight 300 -->
 <link
   href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap"
   rel="stylesheet"
 />
+
+<!-- Montserrat weight 200 -->
 <link
   href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap"
   rel="stylesheet"
 />
+
+<!-- Montserrat weight 100 -->
 <link
   href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap"
   rel="stylesheet"
 />
 
 <!-- HTML code -->
-<h1>De psychologische impact van COVID-19</h1>
-<h2>Breng de indicatoren van angst en depressie in beeld</h2>
+<section>
+  <h1>De psychologische impact van COVID-19</h1>
+  <h2>Breng de indicatoren van angst en depressie in beeld</h2>
+</section>
 
 <!-- Styling -->
 <style>
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 0;
+    padding: 0;
+  }
+
   h1,
   h2 {
     display: flex;
     justify-content: center;
+    align-items: center;
     text-align: center;
   }
 
@@ -32,18 +52,20 @@
     font-optical-sizing: auto;
     font-weight: 300;
     font-style: normal;
-    font-size: 45px;
+    font-size: 30px;
     padding: 5px; /* Ruimte tussen tekst en rand */
+    width: 585x;
+    margin-top: 15px;
+    margin-bottom: 9px;
 
     /* Gradient voor de tekst */
     background: linear-gradient(to right, #40e0d0, #a9a9a9, #00008b, #800080);
     -webkit-background-clip: text; /* Voor Webkit browsers (zoals Safari) */
     background-clip: text; /* Voor andere moderne browsers */
-    color: transparent; /* Maak de tekst transparant zodat de gradient zichtbaar wordt */
+    color: transparent; /* de tekst transparant zodat de gradient zichtbaar wordt */
 
     /* Gradient voor de rand */
-    border: 10px solid transparent; /* Transparante rand */
-    border-radius: 30px; /* Ronde hoeken */
+    border: 5px solid transparent; /* Transparante rand */
     border-image: linear-gradient(to right, #40e0d0, #a9a9a9, #00008b, #800080)
       1; /* Gradient rand */
   }
@@ -53,6 +75,8 @@
     font-optical-sizing: auto;
     font-weight: 200;
     font-style: normal;
-    font-size: 25px;
+    font-size: 18px;
+    margin-top: 0;
+    margin-bottom: 10;
   }
 </style>
