7cce7a03043b3ed2dc2cae45d808a52856f0ef31
kim <Kimvz19@users.noreply.github.com>
2024-11-28 11:48:20 +0100

    adding comments

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -23,13 +23,18 @@
 />
 
 <!-- HTML code -->
+<!-- Titel en subtitel-->
 <section class="header">
   <h1>De psychologische impact van COVID-19</h1>
-  <h2>Meetpunten statistieken van zelfverzekerheid - symtomen anxiety and Depression</h2>
+  <h2>
+    Meetpunten statistieken van zelfverzekerheid - symtomen anxiety and
+    Depression
+  </h2>
 </section>
 
 
-<!-- Styling -->
+
+<!-- Styling CSS-->
 <style>
   .header {
     display: flex;
@@ -50,12 +55,10 @@
 
   h1 {
     font-family: "Montserrat";
-    font-optical-sizing: auto;
     font-weight: 300;
     font-style: normal;
     font-size: 30px;
-    padding: 5px; /* Ruimte tussen tekst en rand */
-    width: 585x;
+    padding: 5px; 
     margin-top: 15px;
     margin-bottom: 9px;
 
@@ -63,10 +66,10 @@
     background: linear-gradient(to right, #40e0d0, #a9a9a9, #00008b, #800080);
     -webkit-background-clip: text; /* Voor Webkit browsers (zoals Safari) */
     background-clip: text; /* Voor andere moderne browsers */
-    color: transparent; /* de tekst transparant zodat de gradient zichtbaar wordt */
+    color: transparent; 
 
     /* Gradient voor de rand */
-    border: 5px solid transparent; /* Transparante rand */
+    border: 5px solid transparent; 
     border-image: linear-gradient(to right, #40e0d0, #a9a9a9, #00008b, #800080)
       1; /* Gradient rand */
   }
@@ -77,7 +80,7 @@
     font-weight: 200;
     font-style: normal;
     font-size: 18px;
-    margin-top: 0;
-    margin-bottom: 10;
+    margin-top: 0px;
+    margin-bottom: 10px;
   }
 </style>
